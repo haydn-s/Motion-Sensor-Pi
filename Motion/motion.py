b6ae95be18c78d97a83b2a4b756d3da1a3754b87
haydn-s <89552939+haydn-s@users.noreply.github.com>
2021-12-30 13:39:59 -0500

    Comment to Indicate Filename Direction

--- Motion/motion.py
+++ Motion/motion.py
@@ -29,7 +29,7 @@ import mutagen.mp3
 import time
 
 # Initialize Audio File to be Played
-file = "ludicrous_speed_go.mp3"
+file = #"insert filename here"
 
 # Use Mutagen to set the proper Sample Rate for the File
 mp3 = mutagen.mp3.MP3(file)
@@ -49,14 +49,14 @@ pir = MotionSensor(4)
 signal_led.off()
 armed_led.off()
 disarmed_led.off()
-# 
+#
 # Helper Functions to handle Motion and No Motion
 def motion():
 #     print("Motion Detected!\n")
     signal_led.on()
     pygame.mixer.music.play()
     time.sleep(10)
-    
+
 def no_motion():
 #     print("Motion Stopped!\n")
     signal_led.off()
@@ -73,24 +73,24 @@ while True:
         armed = not armed
         time.sleep(0.1)
 #         print(str(armed) + "\n")
-    
+
     # Protocol for Armed System
     if armed == True:
 #         print("System is armed...\n")
         disarmed_led.off()
         armed_led.on()
-        
+
         pir.when_motion = motion
         pir.when_no_motion = no_motion
-        
+
         time.sleep(0.1)
     # Protocol for Unarmed System
-    else:  
+    else:
 #         print("System is disarmed...\n")
         armed_led.off()
         disarmed_led.on()
-        
+
         pir.when_motion = None
         pir.when_no_motion = None
-        
+
         time.sleep(0.1)
